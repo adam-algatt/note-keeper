77ee15d6cf4b4a5b2eec985e2967a220b0248379
adam-algatt <adam.algatt@gmail.com>
2023-02-09 11:35:20 -0800

    front-end api fetch functions added

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { addNote, getAllNotes, getSingleNote, updateNoteTitle, updateNoteContent, updateEntireNote, deleteNote} = require('../controllers/notes-controller')
 
 router.route('/').post(addNote);
-router.route('/renameTitle/:noteId').put(updateNoteTitle);
+router.route('/updateTitle/:noteId').put(updateNoteTitle);
 router.route('/updateContent/:noteId').put(updateNoteContent);
 router.route('/updateNote/:noteId').put(updateEntireNote);
 router.route('/').get(getAllNotes)
